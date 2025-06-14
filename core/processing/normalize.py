4f217b746e003b73305c18453fcffbf6e1e9846f
Pablo <pabloprietoroca@gmail.com>
2025-06-14 15:16:32 +0100

    Updated name of function to capital Normalize

--- core/processing/normalize.py
+++ core/processing/normalize.py
@@ -1,4 +1,15 @@
 from .base import Processor
 
-class normalize():
-    pass
+class Normalize():
+    def process(self, data, context=None):
+        """
+        Usage:
+        This processor normalizes the input data by dividing each scan by the maximum value of that scan.
+        """
+        if context is not None and  context["experiment_first_peak"] is not None:
+            print(f"Experiment first peak: {context['experiment_first_peak']}")
+            # Normalize each scan by the first peak value
+            normalized_data = data / context["experiment_first_peak"]
+        else:
+            print("No experiment first peak found in context, normalization cannot proceed")
+        return normalized_data
