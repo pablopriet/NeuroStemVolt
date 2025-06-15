e00d75a7d39dc9e0e6f28b2a1733c80100312190
Pablo <pabloprietoroca@gmail.com>
2025-06-15 12:27:06 +0100

    3D plotting of Color Plot implemented

--- core/spheroid_experiment.py
+++ core/spheroid_experiment.py
@@ -134,6 +134,7 @@ if __name__ == "__main__":
     print(f"Number of files (timepoints) in this experiment: {experiment.get_file_count()}")
     print(f"First file used for baseline: {experiment.get_spheroid_file(3).get_filepath()}")
     experiment.get_spheroid_file(15).visualize_color_plot_data(title_suffix="Baseline")
+    experiment.get_spheroid_file(15).visualize_3d_color_plot(title_suffix="Raw Data")
     experiment.get_spheroid_file(15).visualize_IT_profile()
     experiment.get_spheroid_file(15).visualize_IT_with_exponential_decay()
     metadata = experiment.get_spheroid_file(1).get_metadata()
